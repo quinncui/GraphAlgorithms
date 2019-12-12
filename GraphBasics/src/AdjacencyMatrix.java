7dbb707d7a57e3257f23e9c4c0d1a14628cb2c4b
freekun <836783712@qq.com>
2019-12-12 13:36:16 -0500

    adjacency matrix

--- GraphBasics/src/AdjacencyMatrix.java
+++ GraphBasics/src/AdjacencyMatrix.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class AdjacencyMatrix {
@@ -12,11 +13,29 @@ public class AdjacencyMatrix {
         File file = new File(filename);
         try (Scanner scanner = new Scanner(file)){
             V = scanner.nextInt();
+            if (V < 0) {
+                throw new IllegalArgumentException("V must be non-negative!");
+            }
             E = scanner.nextInt();
+            if (E < 0) {
+                throw new IllegalArgumentException("E must be non-negative!");
+            }
             adjacencyMatrix = new int[V][V];
             for (int i = 0; i < E; i++) {
                 int a = scanner.nextInt();
+                // vertex a should be 0 <= a < V
+                validateVertex(a);
                 int b = scanner.nextInt();
+                // vertex b should be 0 <= b < V
+                validateVertex(b);
+                // detect self loop of a and b
+                if (a == b) {
+                    throw new IllegalArgumentException("Self Loop is Detected!");
+                }
+                // detect parallel edges
+                if (adjacencyMatrix[a][b] == 1) {
+                    throw new IllegalArgumentException("Parallel Edges are detected!");
+                }
                 adjacencyMatrix[a][b] = 1;
                 adjacencyMatrix[b][a] = 1;
             }
@@ -24,6 +43,51 @@ public class AdjacencyMatrix {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private void validateVertex(int v) {
+        if (v < 0 || v >= V) {
+            throw new IllegalArgumentException("vertex " + v + " is invalid");
+        }
+    }
+
+    public int getV() {
+        return V;
+    }
+
+    public int getE() {
+        return E;
+    }
+
+    public boolean hasEdge(int v, int w) {
+        validateVertex(v);
+        validateVertex(w);
+        return adjacencyMatrix[v][w] == 1;
+    }
+
+    /**
+     * get neighbors of the vertex v
+     * @param v
+     * @return
+     */
+    public ArrayList<Integer> adj(int v) {
+        validateVertex(v);
+        ArrayList<Integer> result = new ArrayList<>();
+        for (int i = 0; i < V; i++) {
+            if (adjacencyMatrix[v][i] == 1) {
+                result.add(i);
+            }
+        }
+        return result;
+    }
+
+    /**
+     * get degree of the vertex v
+     * @param v
+     * @return
+     */
+    public int degree(int v) {
+        return adj(v).size();
     }
 
     @Override
