70e43ffa1d6e6f3c00e9a3003a7fe2d82a344be9
freekun <836783712@qq.com>
2019-12-12 22:09:07 -0500

    all pairs path

---
+++ GraphDFS/src/AllPairsPath.java
@@ -0,0 +1,35 @@
+
+public class AllPairsPath {
+
+    private Graph graph;
+
+    private SingleSourcePath[] paths;
+
+    public AllPairsPath(Graph graph) {
+        this.graph = graph;
+
+        paths = new SingleSourcePath[graph.getV()];
+
+        for (int v = 0; v < graph.getV(); v++) {
+            paths[v] = new SingleSourcePath(graph, v);
+        }
+    }
+
+    public boolean isConnectedTo(int s, int t) {
+        graph.validateVertex(s);
+        return paths[s].isConnectedTo(t);
+    }
+
+    public Iterable<Integer> path(int s, int t) {
+        graph.validateVertex(s);
+        return paths[s].path(t);
+    }
+
+    public static void main(String[] args) {
+        Graph graph = new Graph("GraphDFS/graph.txt");
+        AllPairsPath allPairsPath = new AllPairsPath(graph);
+        for (int i = 0; i < allPairsPath.paths.length; i++) {
+            System.out.println(allPairsPath.paths[i].path(6));
+        }
+    }
+}
