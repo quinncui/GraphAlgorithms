5b4d29bc227eae4593a6cf3f8be0326098e1ddda
freekun <836783712@qq.com>
2019-12-13 10:03:12 -0500

    cycle detection

---
+++ GraphDFS/src/CycleDetection.java
@@ -0,0 +1,56 @@
+public class CycleDetection {
+
+    private Graph graph;
+    private boolean[] visited;
+    private boolean hasCylcle = false;
+
+    public CycleDetection(Graph graph) {
+        this.graph = graph;
+        visited = new boolean[graph.getV()];
+        // traverse different components
+        for (int v = 0; v < graph.getV(); v++) {
+            if (!visited[v]) {
+                if (dfs(v, v)) {
+                    hasCylcle = true;
+                    break; // if a cycle already detected in the graph, break;
+                }
+            }
+        }
+    }
+
+    /**
+     * starting from the vertex to detect if there is a cycle in the graph.
+     * if dfs(w, vertex) return true, means there is a cycle starting from w
+     * so there is a cycle starting from vertex, we can return true.
+     * @param vertex
+     * @param parent
+     * @return
+     */
+    private boolean dfs(int vertex, int parent) {
+        visited[vertex] = true;
+        for (int w : graph.neighbors(vertex)) {
+            if (!visited[w]) {
+                if (dfs(w, vertex)) {
+                    return true;
+                }
+            } else if (w != parent){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean hasCycle() {
+        return hasCylcle;
+    }
+
+    public static void main(String[] args) {
+        Graph graph = new Graph("GraphDFS/graph.txt");
+        CycleDetection cycleDetection = new CycleDetection(graph);
+        System.out.println(cycleDetection.hasCylcle);
+
+        Graph graph2 = new Graph("GraphDFS/graph2.txt");
+        CycleDetection cycleDetection2 = new CycleDetection(graph2);
+        System.out.println(cycleDetection2.hasCylcle);
+    }
+}
---
+++ README.md
@@ -0,0 +1,20 @@
+#### Graph Algorithm
+
+##### 1. Graph Basics
+###### 1.1 Adjacency Matrix
+###### 1.2 Adjacency List
+###### 1.3 Adjacency Set
+
+##### 2. Graph DFS
+###### 2.1 Graph (Adjacency Set)
+###### 2.2 Graph DFS (Recursion)
+###### 2.3 Graph DFS Iteration
+###### 2.4 Components
+###### 2.5 Single Source Path
+###### 2.6 All Pairs Path
+###### 2.7 Path
+###### 2.8 Cycle Detection
+
+
+
+##### 3. Graph BFS
